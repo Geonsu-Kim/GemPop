1931bd9cf1a6c5926525f503907290f3adc50186
Geonsu-Kim <33540555+Geonsu-Kim@users.noreply.github.com>
2021-02-15 21:15:33 +0900

    .

---
+++ Assets/Script/Stage/StageBuilder.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageBuilder
+{
+    private int mStageNumber;
+    public StageBuilder(int stageNumber)
+    {
+        mStageNumber = stageNumber;
+    }
+    public Stage ComposeStage(int row,int col)
+    {
+        Stage stage = new Stage(row, col);
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                stage.MBlocks[i, j] = SpawnBlockForStage(i, j);
+                stage.MCells[i, j] = SpawnCellForStage(i, j);
+            }
+        }
+        return stage;
+    }
+    public Stage BuildStage(int stageNum,int row,int col)
+    {
+        return ComposeStage(row, col);
+    }
+    Block SpawnBlockForStage(int row,int col)
+    {
+        return new Block(BlockType.BASIC);
+    }
+    Cell SpawnCellForStage(int row,int col)
+    {
+        return new Cell(CellType.BASIC);
+    }
+}

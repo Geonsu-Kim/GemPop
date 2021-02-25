578f9287dc6d021260ed4c61a7c695d3b28acbaf
Geonsu-Kim <33540555+Geonsu-Kim@users.noreply.github.com>
2021-02-25 21:36:29 +0900

    .

--- Assets/Script/GameMechanism/DataControl/StageInfoList.cs
+++ Assets/Script/GameMechanism/DataControl/StageInfoList.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public static class StageInfoList
 {
     public static List<StageInfo> InfoList= new List<StageInfo>();
+    public static List<StageRecord> recordList = new List<StageRecord>();
+    public static bool binit = false;
+    static int selectedNumber=1;
     public static void Initialization()
     {
+        if (binit) return;
+        binit = true;
         int stageCnt = 0;
         
         while (true)
         {
-            StageInfo info=StageReader.LoadStage(stageCnt);
+            StageInfo info=StageReader.LoadStage(++stageCnt);
             if (info == null) break;
+
             InfoList.Add(info);
+            recordList.Add(new StageRecord());
         }
+    }
+
+    public static int GetNumber()
+    {
+        return selectedNumber;
+    }
+    public static void SetNumber(int num)
+    {
+        selectedNumber = num;
     }
 }

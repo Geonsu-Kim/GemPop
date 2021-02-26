78eecf33e689a445c7d959a0296ee790c4078d5a
Geonsu-Kim <33540555+Geonsu-Kim@users.noreply.github.com>
2021-02-26 18:38:16 +0900

    매치 가능한 블럭 체크 오류 수정

--- Assets/Script/GameMechanism/Board/Board.cs
+++ Assets/Script/GameMechanism/Board/Board.cs
@@ -42,7 +42,6 @@ public class Board
             CheckDeadlock(checkDeadlockAgain);
         } while (!checkDeadlockAgain.value);
 
-        mShuffler.Shuffle();
         for (int i = 0; i < mRow; i++)
         {
             for (int j = 0; j < mCol; j++)
@@ -409,13 +408,13 @@ public class Board
             {
                 if (j + 1 < mCol)
                 {
-                    if (mCells[i, j].MType == CellType.EMPTY || mCells[i, j + 1].MType == CellType.EMPTY) continue;
+                    if (mCells[i, j].MType == CellType.BASIC && mCells[i, j + 1].MType == CellType.BASIC)
                     matchable.value = CheckMatchableHorz(mBlocks[i, j], mBlocks[i, j + 1], i, j, i, j + 1);
                 }
                 if (matchable.value) return;
                 if (i + 1 < mRow)
                 {
-                    if (mCells[i, j].MType == CellType.EMPTY || mCells[i+1, j].MType == CellType.EMPTY) continue;
+                    if (mCells[i, j].MType == CellType.BASIC && mCells[i+1, j].MType == CellType.BASIC)
                     matchable.value = matchable.value || CheckMatchableVert(mBlocks[i, j], mBlocks[i+1, j], i, j, i+1, j);
                 }
                 if (matchable.value) return;

e0aaf40b7ca5020621647541682a68324a0a7f72
Geonsu-Kim <33540555+Geonsu-Kim@users.noreply.github.com>
2021-02-28 21:27:35 +0900

    구글 로그인

--- Assets/Script/InitGame.cs
+++ Assets/Script/InitGame.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using GooglePlayGames;
 public class InitGame : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        PlayGamesPlatform.Activate();
         StageInfoList.Initialization();
 
         SceneManager.LoadScene("scLobby");
--- Assets/Script/UI/LobbyUIManager.cs
+++ Assets/Script/UI/LobbyUIManager.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using GooglePlayGames;
 using TMPro;
 public class LobbyUIManager : SingletonBase<LobbyUIManager>
 {
     public GameObject stageButton;
     public Transform StageSelectMenu;
+    public TextMeshProUGUI LogText;
     public void Start()
     {
         for (int i = 0; i < StageInfoList.recordList.Count; i++)
@@ -20,5 +22,19 @@ public class LobbyUIManager : SingletonBase<LobbyUIManager>
    public void OnExitButton()
     {
         Application.Quit();
+    }
+    public void OnLogin()
+    {
+        Social.localUser.Authenticate((bool _bSuccess) =>
+        {
+            if (true == _bSuccess)
+            {
+                LogText.text = "Success!";
+            }
+            else
+            {
+                LogText.text = "Fail...";
+            }
+        });
     }
 }
